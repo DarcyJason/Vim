ddccec7f8117e12b8fcead2ab83c62f816621092
Homery <139947991+DarcyJason@users.noreply.github.com>
2024-08-25 09:43:09 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd && git clone https://github.com/DarcyJason/Vim.git
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp ~/Vim/vimrc ~/.vimrc
+
+rm -rf ~/Vim
+
+vim -u ~/.vimrc
