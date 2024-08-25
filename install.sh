14ecbe4c07adac300ce1ea950b28ee9770a0d6bc
Homery <139947991+DarcyJason@users.noreply.github.com>
2024-08-25 10:00:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cd && git clone https://github.com/DarcyJason/Vim.git
 
+cat ~/Vim/README.md
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp ~/Vim/vimrc ~/.vimrc
